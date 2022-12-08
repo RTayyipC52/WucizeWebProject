1f01e2c0581285aa24a499318b7a7a8554eb2742
RTayyipC52 <tayyipcicek52@gmail.com>
2022-12-08 13:53:39 +0300

    Add project files.

---
+++ WucizeWebProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WucizeWebProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
